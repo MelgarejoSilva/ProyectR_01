ff75a3e111c02ebbce4de1e3bd7555d50019277b
Diego <Diego@DESKTOP-L37HRLD>
2023-10-02 09:09:28 -0500

    puch2

--- src/main/java/com/example/ProyectR_01/Dulces.java
+++
@@ -1,6 +0,0 @@
-
-package com.example.ProyectR_01;
-
-public class Dulces {
-    int precio;
-}
---
+++ src/main/java/com/example/ProyectR_01/clases/Producto.java
@@ -0,0 +1,9 @@
+package com.example.ProyectR_01.clases;
+
+public class Producto {
+    String nombre;
+    double precio;
+    String descripcion;
+    
+    
+}
