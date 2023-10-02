8f0fa20f96f91e833463ea8d3aebba5a4d2ac30c
Diego <Diego@DESKTOP-L37HRLD>
2023-10-02 06:45:48 -0500

    Inicio

---
+++ src/main/java/com/example/ProyectR_01/ProyectR01Application.java
@@ -0,0 +1,13 @@
+package com.example.ProyectR_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectR01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectR01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProyectR_01/ProyectR01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProyectR_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectR01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
